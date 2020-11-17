31b5d07a5997bb6a2ec4acd6ce2c64ff83675098
Christopher Strickland <cstric12@utk.edu>
2020-11-17 17:06:18 -0500

    Some more random feature changes

--- hello_world.py
+++ hello_world.py
@@ -7,7 +7,7 @@ print("Hello World!!!")
 print("Hello again!")
 
 x = 1
-y = 0
+y = 3
 A = np.array([[1,2,3],[4,5,6]])
 z = x+y
 print(z)
@@ -18,7 +18,8 @@ print(a)
 # features!
 print("here is a new feature!")
 mult_result = x*y
-print(mult_result)
+power = 5
+print(mult_result**power)
 # rarara
 
 assert A.shape == (3,2), "Wrong shape for array A!"
